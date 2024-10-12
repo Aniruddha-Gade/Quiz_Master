0f43dd3791b6819604b4d2df22e66a8d73405020
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-10-12 12:35:53 +0530

    setup mongoDB databse

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { app } from "./app"
+import connectDB from "./utils/database";
 require("dotenv").config();
 
 
@@ -9,4 +10,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("=================================")
     console.log(`Server started on PORT ${PORT}`)
+    connectDB()
 })
